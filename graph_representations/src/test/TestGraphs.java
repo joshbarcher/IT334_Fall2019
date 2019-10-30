0988202ab374e1993cef6499fcb7a88d15ebc2c9
Josh Archer <jarcher@greenriver.edu>
2019-10-30 12:09:22 -0700

    Added some test code the calls the methods in our graph class. This code is meant to be tested using the debugger tool in the IDE.

--- graph_representations/src/test/TestGraphs.java
+++ graph_representations/src/test/TestGraphs.java
@@ -1,6 +1,9 @@
 package test;
 
 import graphs.DirectedAlGraph;
+import graphs.Edge;
+
+import java.util.Set;
 
 public class TestGraphs
 {
@@ -8,12 +11,31 @@ public class TestGraphs
     {
         //generate a new graph
         DirectedAlGraph<Character> letterGraph = new DirectedAlGraph<>();
-        letterGraph.addVertices('a', 'b', 'c', 'd');
+        letterGraph.addVertices('a', 'b', 'c', 'd', 'e');
+        letterGraph.addEdges(
+            new Edge<>('a', 'b'),
+            new Edge<>('a', 'c'),
+            new Edge<>('a', 'e'),
+            new Edge<>('b', 'c'),
+            new Edge<>('c', 'd'),
+            new Edge<>('c', 'e'),
+            new Edge<>('d', 'a'),
+            new Edge<>('d', 'b'),
+            new Edge<>('d', 'e')
+        );
 
-        for (char character : letterGraph.vertices())
-        {
-            char upperCharacter = (char)(character - 32);
-            letterGraph.addVertex(upperCharacter);
-        }
+        Set<Character> vertices = letterGraph.vertices();
+        Set<Edge<Character>> edges = letterGraph.edges();
+
+        boolean hasA = letterGraph.containsVertex('a'); //should be true
+        boolean hasF = letterGraph.containsVertex('f'); //should be false
+
+        boolean hasAB = letterGraph.containsEdge(new Edge<>('a', 'b')); //should be true
+        boolean hasAF = letterGraph.containsEdge(new Edge<>('a', 'f')); //should be false
+
+        //all of the following should throw an exception
+        letterGraph.addEdge(new Edge<>('a', 'a')); //self-loop
+        letterGraph.addEdge(new Edge<>('f', 'g')); //missing vertex
+        letterGraph.addEdge(new Edge<>('a', 'c')); //duplicate edge
     }
 }
